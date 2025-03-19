0a874637fc2d4c09dfd720152e1808d97543a746
Cristina Garcia Cardona <cgarciac@lanl.gov>
2025-03-19 16:28:33 -0600

    Add elephant skeleton example

---
+++ rmc/examples/elephant_skeleton2D_HMC.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example of 2D Skeleton Distribution
+===================================
+
+This script includes demonstrates the usage of HMC class for sampling
+from a 2D skeleton given by data from a file.
+"""
+
+from functools import partial
+from typing import Optional
+
+import numpy as np
+
+import jax
+import jax.numpy as jnp
+from jax.typing import ArrayLike
+
+from jax.random import multivariate_normal
+
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from rmc import ConfigDict, Energy, HMC, LinearRegressionE
+
+RealArray = ArrayLike
+
+"""
+Define energy function
+"""
+class ESkeleton2D(Energy):
+    def __init__(self, z, sigma):
+        self.z = jnp.array(z)
+        self.sigma = sigma
+        self.numd, self.dim = self.z.shape
+
+    def log_prior(self, x: RealArray) -> RealArray:
+        return 0.
+
+    def mvnpdfsum(self, x, i, psum):
+        return psum + jax.scipy.stats.multivariate_normal.pdf(x, mean=self.z[i, :], cov=jnp.eye(self.dim) * self.sigma**2)
+
+    def log_likelihood(self, x: RealArray) -> RealArray:
+        funcbody = partial(self.mvnpdfsum, jnp.ravel(x))
+        p = jax.lax.fori_loop(0, self.numd, funcbody, 0.)
+        ll = jnp.log(p) - jnp.log(self.numd)
+        return ll.squeeze()
+
+
+"""
+Define distribution.
+"""
+z = np.load("examples/elephantz.npy")
+D = z.shape[0]             # Number of points in skeleton
+d = z.shape[1]             # Dimension of points in skeleton
+
+"""
+Configure sampling run.
+"""
+# random generation
+key = jax.random.PRNGKey(3)
+key, x_key = jax.random.split(key)
+key, call_key = jax.random.split(key)
+
+sigma = 0.02
+Ecl = ESkeleton2D(z, sigma)
+
+# sampling configuration
+prior_mean = 0.1
+prior_std = 0.5
+smp_conf: ConfigDict = {
+    "seed": 0,
+    "sample_shape": (1, d),
+    "initial_sampler_fn": multivariate_normal,
+    "initial_sampler_mean": prior_mean * jnp.ones((1, d)),
+    "initial_sampler_covariance": jnp.diagflat((prior_std * jnp.ones((d,)))**2).reshape((1, d, d)),
+    "maxiter": 300,
+    "numsteps": 100,
+    "log_freq": 1,
+    "energy_cl": Ecl,
+    "step_size": 0.01,
+    "store_path": True,
+}
+print(f"Sampling configured --> parameters: {smp_conf}")
+
+"""
+Construct sampling object.
+"""
+hmc_obj = HMC(smp_conf)
+print("HMC object constructed")
+
+"""
+Run sampler.
+"""
+hmc_obj.sample()
+
+"""
+Plot all samples, HMC samples and corresponding trajectories.
+"""
+qpath = jnp.array(hmc_obj.qpath)
+print("Collected samples: ", qpath.shape)
+
+from scipy.stats import multivariate_normal as scipymvn
+
+samples = []
+for i in range(2000):
+    ind = np.random.choice(D, size=1)
+    m = z[ind,:].squeeze()
+    samples.append(scipymvn(mean=m, cov=np.eye(d)*sigma**2).rvs(size=1))
+samples = np.array(samples)
+
+
+from matplotlib import pyplot as plt, cm
+plt.rcParams.update({'font.size':16})
+colors = cm.plasma(np.linspace(0, 1, 12))
+
+fig,ax = plt.subplots(1, 1, figsize=(9,5))
+ax.scatter(samples[:, 0], samples[:, 1], s=1, marker = 'o', color = colors[8], label = 'MC samples', zorder=0)
+ax.axis('equal')
+
+for i in range(smp_conf["maxiter"]):
+    ax.plot(qpath[i][:,0,0], qpath[i][:,0,1], color = 'k', linestyle=':', label='HMC trajectory' if i==0 else None, zorder = 1)
+    ax.scatter(qpath[i][0,0,0], qpath[i][0,0,1], edgecolor=[], facecolor = colors[0], label='HMC samples' if i==0 else None, zorder = 2)
+
+ax.scatter(qpath[-1][-1,0,0], qpath[-1][-1,0,1], edgecolor=[], facecolor = colors[0])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.legend(loc=2,frameon=False)#,bbox_to_anchor=(1.0, 0.7))
+plt.show()
+
