6e201e9f6fe4f4486a568576879748287e39f189
crstngc <cristina.cgarcia@gmail.com>
2026-01-17 16:02:35 -0700

    Added normal packed distributions

--- rmc/test/test_packdist.py
+++ rmc/test/test_packdist.py
@@ -1,6 +1,41 @@
-import pytest
+import jax
+import jax.numpy as jnp
+from jax.scipy.stats import uniform
 
-from rmc.utils.packed_distributions import BasePackedDistribution
+import numpy as np
+import pytest
+from scipy.stats import multivariate_normal as scipymvnorm
+from scipy.stats import norm as scipynorm
+
+from rmc.utils.packed_distributions import (
+    BasePackedDistribution,
+    PackedMultivariateNormal,
+    PackedNormal,
+    PackedUniform,
+)
+
+
+class SetupTest:
+    def __init__(self):
+        class Foo(BasePackedDistribution):
+            def __init__(self, scale):
+                self.scale = scale
+
+            def log_pdf(self, x):
+                return uniform.logpdf(x, scale=self.scale)
+
+            def rvs(self, shape, key):
+                return jax.random.uniform(key, shape, maxval=self.scale)
+
+        class Foo2(BasePackedDistribution):
+            def __init__(self, scale):
+                self.scale = scale
+
+            def rvs(self, shape, key):
+                return jax.random.uniform(key, shape, maxval=self.scale)
+
+        self.Foo = Foo
+        self.Foo2 = Foo2
 
 
 def test_base_packed_dist_exception():
@@ -8,8 +43,95 @@ def test_base_packed_dist_exception():
         BasePackedDistribution()
 
 
-# def test_base_packed_dist_log_pdf_exception():
-#    class TestPDist(BasePackedDistribution):
+@pytest.fixture(scope="module")
+def testobj():
+    yield SetupTest()
 
-#    with pytest.raises(TypeError):
-#        BasePackedDistribution()
+
+def test_create_Foo(testobj):
+    scale = 0.5
+    try:
+        testobj.Foo(scale)
+    except Exception as e:
+        print(e)
+        assert 0
+
+
+def test_base_packed_dist_log_pdf_exception(testobj):
+    scale = 0.5
+
+    with pytest.raises(TypeError):
+        testobj.Foo2(scale)
+
+
+def test_create_uniform():
+    try:
+        PackedUniform()
+    except Exception as e:
+        print(e)
+        assert 0
+
+
+def test_create_normal():
+    try:
+        PackedNormal()
+    except Exception as e:
+        print(e)
+        assert 0
+
+
+def test_normal_logpdf():
+    # Create normal distribution
+    mean = 10.3
+    stddev = 0.5
+    obj = PackedNormal(mean, stddev)
+    # Generate uniform samples
+    shape = (100,)
+    key = jax.random.key(4444)
+    x = jax.random.uniform(key, shape, maxval=2 * mean)
+    # Compute log pdf from packed distribution
+    logpdf = obj.log_pdf(x)
+    # Compute log pdf from scipy
+    logpdf_scipy = scipynorm.logpdf(x, loc=mean, scale=stddev)
+
+    np.testing.assert_allclose(logpdf, logpdf_scipy, rtol=1e-6)
+
+
+def test_normal_sampling():
+    # Create normal distribution
+    mean = 0.3
+    stddev = 0.9
+    obj = PackedNormal(mean, stddev)
+    # Define shape for sampling
+    shape = (10_000_000,)
+    # Sample from packed distribution
+    key = jax.random.key(4444)
+    samples = obj.rvs(shape, key)
+    mean_samples = jnp.mean(samples)
+    stddev_samples = jnp.std(samples)
+    # Sample from scipy
+    samples_scipy = scipynorm.rvs(loc=mean, scale=stddev, size=shape)
+    mean_samples_scipy = jnp.mean(samples_scipy)
+    stddev_samples_scipy = jnp.std(samples_scipy)
+
+    np.testing.assert_allclose(mean_samples, mean_samples_scipy, rtol=2e-3)
+    np.testing.assert_allclose(stddev_samples, stddev_samples_scipy, rtol=2e-3)
+
+
+def test_multivariate_normal_logpdf():
+    key = jax.random.key(1234)
+    key, subkey = jax.random.split(key)
+    # Create multivariate normal distribution
+    D = 2
+    mean = jax.random.normal(subkey, D)
+    cov = jnp.diag(jnp.array([0.5, 0.2]))
+    obj = PackedMultivariateNormal(mean, cov)
+    # Generate uniform samples
+    shape = (100, D)
+    x = jax.random.uniform(key, shape)
+    # Compute log pdf from packed distribution
+    logpdf = obj.log_pdf(x)
+    # Compute log pdf from scipy
+    logpdf_scipy = scipymvnorm.logpdf(x, mean=mean, cov=cov)
+
+    np.testing.assert_allclose(logpdf, logpdf_scipy, rtol=1e-6)
--- rmc/utils/packed_distributions.py
+++ rmc/utils/packed_distributions.py
@@ -5,6 +5,8 @@
 from abc import ABC, abstractmethod
 from typing import Tuple
 
+import jax
+from jax.scipy.stats import multivariate_normal, norm, uniform
 from jax.typing import ArrayLike
 
 
@@ -15,6 +17,11 @@ class BasePackedDistribution(ABC):
     existing distributions and enable a dual role of sampling and
     computing statistics (e.g. log probability density function).
     """
+
+    # Flag to indicate that one can sample from this class
+    can_sample: bool = True
+    # Flag to indicate that one can evaluate log pdf from this class
+    has_logpdf: bool = True
 
     @abstractmethod
     def __init__(self, **kwargs):
@@ -33,22 +40,141 @@ class BasePackedDistribution(ABC):
         """Computation of the log probability density function (pdf).
 
         Args:
-            x: Array of samples to evaluate log-pdf.
+            x: Array of samples to evaluate log pdf.
 
         Returns:
-            Array of evaluated log-pdf.
+            Array of evaluated log pdf.
         """
         raise NotImplementedError
 
     @abstractmethod
-    def rvs(self, shape: Tuple[int], rngk: ArrayLike) -> ArrayLike:
+    def rvs(self, shape: Tuple[int], key: ArrayLike) -> ArrayLike:
         """Random variable sampling (rvs).
 
         Args:
             shape: Tuple describing the shape of samples to generate.
-            rngk: Key for random number generation.
+            key: Key for random number generation.
 
         Returns:
             Generated samples.
         """
         raise NotImplementedError
+
+
+class PackedNormal(BasePackedDistribution):
+    """Class for sampling and getting statistics from univariate normal
+    distribution."""
+
+    def __init__(self, mean: float = 0.0, stddev: float = 1.0):
+        """Set parameters of univariate normal distribution.
+
+        Args:
+            mean: Mean value of the univariate normal distribution.
+            stddev: Standard deviation of the univariate normal distribution.
+        """
+        self.mean = mean
+        self.stddev = stddev
+        self.loc = mean  # Distribution offset parameter (jax scipy)
+        self.scale = stddev  # Distribution scale parameter (jax scipy)
+
+    def log_pdf(self, x: ArrayLike) -> ArrayLike:
+        """Computation of the log pdf for univariate normal distribution.
+
+        Args:
+            x: Array of samples to evaluate log pdf.
+
+        Returns:
+            Array of evaluated univariate normal log pdf.
+        """
+        return norm.logpdf(x, loc=self.loc, scale=self.scale)
+
+    def rvs(self, shape: Tuple[int], key: ArrayLike) -> ArrayLike:
+        """Random variable sampling (rvs) from univariate normal distribution.
+
+        Args:
+            shape: Tuple describing the shape of samples to generate.
+            key: Key for random number generation.
+
+        Returns:
+            Generated univariate normal distribution samples.
+        """
+        return jax.random.normal(key, shape) * self.stddev + self.mean
+
+
+class PackedMultivariateNormal(BasePackedDistribution):
+    """Class for sampling and getting statistics from multivariate normal
+    distribution."""
+
+    def __init__(self, mean: ArrayLike, cov: ArrayLike):
+        """Set parameters of multivariate normal distribution.
+
+        Args:
+            mean: Mean value of the multivariate normal distribution.
+            cov: Covariance matrix of the multivariate normal distribution.
+        """
+        self.mean = mean
+        self.cov = cov
+
+    def log_pdf(self, x: ArrayLike) -> ArrayLike:
+        """Computation of the log pdf for multivariate normal distribution.
+
+        Args:
+            x: Array of samples to evaluate log pdf.
+
+        Returns:
+            Array of evaluated multivariate normal log pdf.
+        """
+        return multivariate_normal.logpdf(x, mean=self.mean, cov=self.cov)
+
+    def rvs(self, shape: Tuple[int], key: ArrayLike) -> ArrayLike:
+        """Random variable sampling (rvs) from univariate normal distribution.
+
+        Args:
+            shape: Tuple describing the shape of samples to generate.
+            key: Key for random number generation.
+
+        Returns:
+            Generated univariate normal distribution samples.
+        """
+        return jax.random.multivariate_normal(key, self.mean, self.cov, shape)
+
+
+class PackedUniform(BasePackedDistribution):
+    """Class for sampling and getting statistics from uniform distribution."""
+
+    def __init__(self, minval: float = 0.0, maxval: float = 1.0):
+        """Set parameters of uniform distribution.
+
+        Args:
+            minval: Inclusive minimum value to generate in the samples of
+                the uniform distribution.
+            maxval: Exclusive maximum value to generate in the samples of
+                the uniform distribution.
+        """
+        self.minval = minval
+        self.maxval = maxval
+        self.loc = minval  # Distribution offset parameter (jax scipy)
+        self.scale = maxval - minval  # Distribution scale parameter (jax scipy)
+
+    def log_pdf(self, x: ArrayLike) -> ArrayLike:
+        """Computation of the log pdf for uniform distribution.
+
+        Args:
+            x: Array of samples to evaluate uniform log pdf.
+
+        Returns:
+            Array of evaluated uniform log pdf.
+        """
+        return uniform.logpdf(x, loc=self.loc, scale=self.scale)
+
+    def rvs(self, shape: Tuple[int], key: ArrayLike) -> ArrayLike:
+        """Random variable sampling (rvs) from uniform distribution.
+
+        Args:
+            shape: Tuple describing the shape of samples to generate.
+            key: Key for random number generation.
+
+        Returns:
+            Generated uniform distribution samples.
+        """
+        return jax.random.uniform(key, shape, minval=self.minval, maxval=self.maxval)
