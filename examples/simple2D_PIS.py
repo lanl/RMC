27ffc1a493960b775fe3cf249162e5f543d385c5
crstngc <cristina.cgarcia@gmail.com>
2026-04-16 19:54:22 -0600

    Add simple 2D PIS example

---
+++ examples/simple2D_PIS.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example of PIS for 2D Normal Distribution
+=========================================
+
+This script demonstrates the usage of a Path Integral Sampler
+(PIS) for sampling from a skewed 2D Normal distribution.
+"""
+
+
+import os
+import sys
+
+import jax
+import jax.numpy as jnp
+from jax.typing import ArrayLike
+
+import numpy as np
+from flax import nnx
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from utils.density_examples import Norm2D
+
+from rmc import (
+    PackedMultivariateNormal,
+    PathIntegralSampler,
+    plot_quiver,
+    plot_samples,
+    plot_trajectories,
+    save_plot,
+)
+from rmc.flax.nn_config_dict import NNConfigDict
+
+RealArray = ArrayLike
+
+"""
+Set distribution: simple 2D Normal. Use "iso" for isotropic example.
+Otherwise, a lopsided distribution will be sampled.
+"""
+d = 2  # Dimension of x/inputs
+cov = jnp.array([[0.2, 0.0], [0.0, 1.0]])  # lopsided Gaussian
+rotationAngle = 7 * jnp.pi / 16
+R = jnp.array(
+    [
+        [jnp.cos(rotationAngle), -jnp.sin(rotationAngle)],
+        [jnp.sin(rotationAngle), jnp.cos(rotationAngle)],
+    ]
+)
+cov = R.dot(cov).dot(R.T).reshape((1, d, d))
+
+Dcl = Norm2D(cov)
+
+"""
+Configure sampling run.
+"""
+# define base distribution
+mean_base = jnp.zeros(d).reshape((1, d))
+cov_base = 1.5 * jnp.eye(d).reshape((1, d, d))
+distribution0 = PackedMultivariateNormal(mean_base, cov_base)
+
+"""
+Construct Path Integral Sampler (PIS) Model, a Flax neural network (NN) model,
+specifically a multi-layer perceptron (MLP).
+"""
+# NN configuration
+layer_widths = [64, 64]  # number of neurons per layer
+nn_conf: NNConfigDict = {
+    "seed": 10,
+    "batch_size": 500,
+    "dim": d,
+    "layer_widths": layer_widths,
+    "activation_func": nnx.silu,
+    "time_embed": False,
+    "opt_type": "ADAM",
+    "base_lr": 1e-2,
+    "max_epochs": 1000,
+    "dt_max": 2e-1,
+    "dist0": distribution0,
+    "max_samples": 10000,
+    "nsamples": 2000,
+    "eval_every": 100,
+    "warm_start": False,
+    "max_loss": 1e-1,
+    "max_subiter": 1,
+    "has_aux": False,
+    "root_path": "./results_pis_s2D/",
+}
+print(f"Path integral sampling configured --> parameters: {nn_conf}")
+
+"""
+Build PIS model.
+"""
+h = 0.1
+T = 10
+PISmodel = PathIntegralSampler(nn_conf, Dcl, h, T, verbose=True)
+print("PIS model constructed")
+
+"""
+Train model.
+"""
+PISmodel.train()
+print("PIS model trained")
+
+"""
+Run sampler.
+"""
+key = jax.random.PRNGKey(nn_conf["seed"])
+key, subkey = jax.random.split(key)
+particle_path_, w = PISmodel.sample(nn_conf["nsamples"], subkey)
+particles = jnp.array(particle_path_)
+print(f"particles shape: {particles.shape}")
+print(f"Evolution of {particles.shape[1]} particles during {particles.shape[0]-1} time steps")
+
+"""
+Plot samples and trajectories.
+"""
+
+from matplotlib import cm
+from matplotlib import pyplot as plt
+
+plt.rcParams.update({"font.size": 16})
+colors = cm.plasma(np.linspace(0, 1, 12))
+
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(11, 11))
+# Plot samples from initial density function
+ax1 = plot_samples(particles[0], ax1, size=5, label="Initial samples", color=colors[8])
+# Plot samples generated from LFIS method
+ax2 = plot_samples(particles[-1], ax2, size=5, label="PIS samples")
+
+# Plot complete time trajectories for 10 samples
+for i in range(10):
+    ax3 = plot_trajectories(
+        particles[:, i, :],
+        ax3,
+        label=None,
+        color="k",
+    )
+    ax3 = plot_samples(particles[:, i, :], ax3, label=f"s{i+1}" if i < 3 else None)
+
+# Plot initial sample and segment time trajectories for 20 samples
+for i in range(20):
+    ax4 = plot_trajectories(
+        particles[:, i, :],
+        ax4,
+        label="trajectory" if i == 0 else None,
+        zorder=1,
+        color="k",
+    )
+    ax4 = plot_samples(
+        jnp.atleast_2d(particles[0, i, :]),
+        ax4,
+        label="Initial samples" if i == 0 else None,
+        size=10,
+        zorder=2,
+        edgecolor=[],
+        facecolor=colors[8],
+    )
+
+# Plot samples generated from PIS method
+# with overimposed segment trajectories for 10 samples
+# and initial position marked as initial samples
+for i in range(10):
+    ax5 = plot_trajectories(
+        particles[:, i, :],
+        ax5,
+        label="trajectory" if i == 0 else None,
+        zorder=1,
+        color="k",
+    )
+    ax5 = plot_samples(
+        jnp.atleast_2d(particles[0, i, :]),
+        ax5,
+        size=30,
+        label="Initial samples" if i == 0 else None,
+        zorder=2,
+        edgecolor=[],
+        facecolor=colors[8],
+    )
+ax5 = plot_samples(
+    particles[-1], ax5, size=20, label="PIS samples", edgecolor=[], facecolor=colors[0]
+)
+
+# Plot model control
+control = PISmodel.nnmodel(particles[-1], h * T)
+ax6 = plot_quiver(particles[-1], control, ax6)
+
+# Save plot
+save_plot(fig, nn_conf["root_path"] + "samplePIS_s2D.png")
